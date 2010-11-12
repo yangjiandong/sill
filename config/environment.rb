635908665759968d642df7054c32b30c5755f79d
yangjiandong <young.jiandong@gmail.com>
2010-11-12 15:12:20 +0800

    first..

---
+++ config/environment.rb
@@ -0,0 +1,19 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# require 'memcache'
+
+# memcache_options = {
+# :c_threshold = 10_00,
+# :compression = true,
+# :debug = false,
+# :namespace = 'yourappname_or_anything_you_like',
+# :readonly = false,
+# :urlencode = false
+# }
+# CACHE = MemCache.new(memcache_options)
+# CACHE.servers = '127.0.0.1:11211'
+# config.action_controller.fragment_cache_store = CACHE, {}
+
+# Initialize the rails application
+Sill::Application.initialize!
