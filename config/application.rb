f60c938c2eb9cab4f5de588c055db9821b627620
young.jiandong <young.jiandong@gmail.com>
2010-12-26 17:34:39 +0800

    del other layouts

--- config/application.rb
+++ config/application.rb
@@ -70,7 +70,11 @@ module Sill
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:jquery] = %w(jquery jquery.rails)
+    if Rails.env.development?
+      config.action_view.javascript_expansions[:jquery] = %w(jquery jquery.rails)
+    else
+      config.action_view.javascript_expansions[:jquery] = %w(jquery.min jquery.rails)
+    end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
