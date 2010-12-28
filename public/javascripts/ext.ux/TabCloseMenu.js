6730cb5330e03eade46df707974ce37a15797470
yangjiandong <young.jiandong@gmail.com>
2010-12-28 17:19:17 +0800

    update..

--- public/javascripts/ext.ux/TabCloseMenu.js
+++ public/javascripts/ext.ux/TabCloseMenu.js
@@ -6,11 +6,11 @@
  */
 /**
  * @class Ext.ux.TabCloseMenu
- * @extends Object 
+ * @extends Object
  * Plugin (ptype = 'tabclosemenu') for adding a close context menu to tabs. Note that the menu respects
  * the closable configuration on the tab. As such, commands like remove others and remove all will not
  * remove items that are not closable.
- * 
+ *
  * @constructor
  * @param {Object} config The configuration options
  * @ptype tabclosemenu
@@ -20,17 +20,17 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
      * @cfg {String} closeTabText
      * The text for closing the current tab. Defaults to <tt>'Close Tab'</tt>.
      */
-    closeTabText: 'Close Tab',
+    closeTabText: '关闭标签',
 
     /**
      * @cfg {String} closeOtherTabsText
      * The text for closing all tabs except the current one. Defaults to <tt>'Close Other Tabs'</tt>.
      */
-    closeOtherTabsText: 'Close Other Tabs',
-    
+    closeOtherTabsText: '关闭其它标签',
+
     /**
      * @cfg {Boolean} showCloseAll
-     * Indicates whether to show the 'Close All' option. Defaults to <tt>true</tt>. 
+     * Indicates whether to show the 'Close All' option. Defaults to <tt>true</tt>.
      */
     showCloseAll: true,
 
@@ -38,8 +38,8 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
      * @cfg {String} closeAllTabsText
      * <p>The text for closing all tabs. Defaults to <tt>'Close All Tabs'</tt>.
      */
-    closeAllTabsText: 'Close All Tabs',
-    
+    closeAllTabsText: '关闭所有标签',
+
     constructor : function(config){
         Ext.apply(this, config || {});
     },
@@ -53,12 +53,12 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
             destroy: this.destroy
         });
     },
-    
+
     destroy : function(){
         Ext.destroy(this.menu);
         delete this.menu;
         delete this.tabs;
-        delete this.active;    
+        delete this.active;
     },
 
     // private
@@ -68,7 +68,7 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
             disableAll = true,
             disableOthers = true,
             closeAll = m.getComponent('closeall');
-        
+
         m.getComponent('close').setDisabled(!item.closable);
         tabs.items.each(function(){
             if(this.closable){
@@ -83,11 +83,12 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
         if(closeAll){
             closeAll.setDisabled(disableAll);
         }
-        
+
         e.stopEvent();
         m.showAt(e.getPoint());
     },
-    
+
+    //右键菜单
     createMenu : function(){
         if(!this.menu){
             var items = [{
@@ -119,26 +120,26 @@ Ext.ux.TabCloseMenu = Ext.extend(Object, {
         }
         return this.menu;
     },
-    
+
     onClose : function(){
         this.tabs.remove(this.active);
     },
-    
+
     onCloseOthers : function(){
         this.doClose(true);
     },
-    
+
     onCloseAll : function(){
         this.doClose(false);
     },
-    
+
     doClose : function(excludeActive){
         var items = [];
         this.tabs.items.each(function(item){
             if(item.closable){
                 if(!excludeActive || item != this.active){
                     items.push(item);
-                }    
+                }
             }
         }, this);
         Ext.each(items, function(item){
