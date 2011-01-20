a29243c2cf5fd05940f5116e1cd0b85e593749b8
yangjiandong <young.jiandong@gmail.com>
2011-01-20 15:19:10 +0800

    migrate hz,update to mssql,but ..

--- db/migrate/20101030031918_create_hzs.rb
+++ db/migrate/20101030031918_create_hzs.rb
@@ -17,7 +17,41 @@ class CreateHzs < ActiveRecord::Migration
       # 0.05s,0.5s
       sql = "INSERT INTO t_hzk (hz,py,wb) VALUES #{inserts.join(", ")}"
       Hz.connection.execute sql
+
+    elsif Hz.connection.adapter_name.downcase == 'mssql'
+      # 分1000条插入,不然报内存不够.效果没mysql好,还需30s
+      inserts = []
+      i=0
+      file = File.open("#{RAILS_ROOT}/db/seeds/hzk.yml", 'r')
+      YAML::load(file).each do |k,record|
+        if (i == 0)
+          inserts =[]
+        end
+        i = i + 1
+
+        # ["hzk_001", {"hz"=>"啊", "py"=>"A", "wb"=>"BS"}]
+        inserts.push " UNION ALL SELECT '#{record['hz']}','#{record['py']}','#{record['wb']}' "
+        if (i == 1000)
+          # pp inserts
+
+          #ar1 = inserts[0]
+          #ar1["UNION ALL SELECT"] = "SELECT"
+          inserts[0][" UNION ALL SELECT"] = " SELECT"
+          sql = "INSERT INTO t_hzk (hz,py,wb) #{inserts.join(" ")}"
+          Hz.connection.execute sql
+          i=0
+        end
+      end
+      
+      if !inserts.empty?
+          # ar1 = inserts[0]
+          inserts[0][" UNION ALL SELECT"] = "SELECT"
+          sql = "INSERT INTO t_hzk (hz,py,wb) #{inserts.join(" ")}"
+          Hz.connection.execute sql
+      end
+
     else
+
       # use ActiveRecord::Extensions
       # http://www.continuousthinking.com/tags/arext
       # ar-extensions
@@ -41,7 +75,7 @@ class CreateHzs < ActiveRecord::Migration
         # Hz.crewait(
           # :hz => record['hz'],
           # :py => record['py'],
-          # :wb => record['wb']
+        # :wb => record['wb']
         # )
 
       end
